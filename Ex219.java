f281bd7eca85b9a6ee68f5e28b7a4c21793a3091
NikitaProkoshin1 <115005189+NikitaProkoshin1@users.noreply.github.com>
2022-12-11 10:33:13 +0700

    Create Ex219.java

---
+++ Ex219.java
@@ -0,0 +1,29 @@
+package JavaHW5;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Ex219 {
+    public static void main(String[] args) {
+        //int[] nums = new int[]{1, 2, 3, 1};
+        //int[] nums = new int[]{1, 0, 1, 1};
+        int[] nums = new int[]{1, 2, 3, 1, 2, 3};
+        System.out.println(containsNearbyDuplicate(nums, 2));
+    }
+
+    public static boolean containsNearbyDuplicate(int[] nums, int k) {
+        Map<Integer, Integer> map = new HashMap<>();
+
+        for (int i = 0; i < nums.length; i++) {
+            if (map.containsKey(nums[i])) {
+                if (i - map.get(nums[i]) <= k) {
+                    return true;
+                }
+            }
+            map.put(nums[i], i);
+        }
+        return false;
+    }
+}
+
+//https://leetcode.com/problems/contains-duplicate-ii/
