afd6a26890b0f461ef53dc66e24c5bbe39f0fe9f
NikitaProkoshin1 <115005189+NikitaProkoshin1@users.noreply.github.com>
2022-12-11 11:05:30 +0700

    Добавлено второе решение

--- Ex013.java
+++ Ex013.java
@@ -31,4 +31,31 @@ public class Ex013 {
     }
 }
 
+
+/*class Solution
+    public int romanToInt(String s) {
+        Map<Character, Integer> map = new HashMap<>();
+        map.put('I', 1);
+        map.put('V', 5);
+        map.put('X', 10);
+        map.put('L', 50);
+        map.put('C', 100);
+        map.put('D', 500);
+        map.put('M', 1000);
+        int sum = 0;
+        for(int i = 0; i < s.length(); i++){
+            if(s.charAt(i) == 'I' && i + 1 < s.length() && (s.charAt(i + 1) == 'V' || s.charAt(i + 1) == 'X')){
+                sum -= map.get(s.charAt(i));
+            }else if(s.charAt(i) == 'X' && i + 1 < s.length() && (s.charAt(i + 1) == 'L' || s.charAt(i + 1) == 'C')){
+                sum -= map.get(s.charAt(i));
+            }else if(s.charAt(i) == 'C' && i + 1 < s.length() && (s.charAt(i + 1) == 'D' || s.charAt(i + 1) == 'M')){
+                sum -= map.get(s.charAt(i));
+            }else{
+                sum += map.get(s.charAt(i));
+            }
+        }
+        return sum;
+    }
+}
+
 //https://leetcode.com/problems/roman-to-integer/
