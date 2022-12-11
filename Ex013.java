b67ceb108d175f0f1c2d7a8e819a62b87e0b559a
NikitaProkoshin1 <115005189+NikitaProkoshin1@users.noreply.github.com>
2022-12-11 10:53:05 +0700

    Create Ex013.java

---
+++ Ex013.java
@@ -0,0 +1,32 @@
+package JavaHW5;
+
+import java.util.HashMap;
+
+public class Ex013 {
+    public static void main(String[] args) {
+        String s = "MCMXXXV";
+        System.out.println(romanToInt(s));
+    }
+    public static int romanToInt(String s) {
+        HashMap<Character, Integer> roman = new HashMap<>();
+        roman.put('I', 1);
+        roman.put('V', 5);
+        roman.put('X', 10);
+        roman.put('L', 50);
+        roman.put('C', 100);
+        roman.put('D', 500);
+        roman.put('M', 1000);
+
+        int res = 0;
+
+        for (int i = 0; i < s.length() - 1; i++) {
+            if (roman.get(s.charAt(i)) < roman.get(s.charAt(i + 1))) {
+                res = res - roman.get(s.charAt(i));
+            }
+            else {
+                res = res + roman.get(s.charAt(i));
+            }
+        }
+        return res + roman.get(s.charAt(s.length() - 1));
+    }
+}
